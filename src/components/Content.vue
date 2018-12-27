e5d12e1437434f62d25647877cada8b3145b45b1
wb-ldj313979 <wb-ldj313979@alibaba-inc.com>
2018-12-27 20:17:11 +0800

    dg

---
+++ src/components/Content.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="content">
+    <img class="content" src=""/>
+    <div>{{msg}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'content',
+  components: {
+  },
+  data () {
+    return {
+      msg: '古德'
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .content {
+    height: 400px;
+    width: 400px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    
+  }
+</style>
